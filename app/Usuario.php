049cb16fbb53866aca125dc380566d6916ef2ed1
ERICKVH66 <erickwith@hotmail.com>
2016-10-12 23:26:20 -0500

    Laravel Heroku
    Commit inicial

---
+++ app/Usuario.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Usuario extends Model
+{
+    protected $table= 'usuario';
+
+    public $timestamps = false;
+    protected $fillable = ['id_usuario','nombre', 'apellido','telefono', 'direcion'];
+
+}
