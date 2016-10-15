0bead91fa38273bd0dc683d74460a4f4049c3fe3
ERICKVH66 <erickwith@hotmail.com>
2016-10-15 11:00:15 -0500

    Data Base Heroku
    Configuracion de archivos para la base de datos en heroku

--- config/database.php
+++ config/database.php
@@ -1,5 +1,9 @@
 <?php
-
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
 return [
 
     /*
@@ -46,37 +50,45 @@ return [
 
     'connections' => [
 
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-        ],
+    'sqlite' => [
+    'driver'   => 'sqlite',
+    'database' => database_path('database.sqlite'),
+    'prefix'   => '',
+    ],
 
-        'mysql' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix' => '',
-            'strict' => false,
-            'engine' => null,
-        ],
+    'mysql' => [
+    'driver'    => 'mysql',
+    'host'      => $host,
+    'database'  => $database,
+    'username'  => $username,
+    'password'  => $password,
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+    'strict'    => false,
+    'engine'    => null,
+    ],
 
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-        ],
+    'pgsql' => [
+    'driver'   => 'pgsql',
+    'host'     => env('DB_HOST', 'localhost'),
+    'database' => env('DB_DATABASE', 'forge'),
+    'username' => env('DB_USERNAME', 'forge'),
+    'password' => env('DB_PASSWORD', ''),
+    'charset'  => 'utf8',
+    'prefix'   => '',
+    'schema'   => 'public',
+    ],
+
+    'sqlsrv' => [
+    'driver'   => 'sqlsrv',
+    'host'     => env('DB_HOST', 'localhost'),
+    'database' => env('DB_DATABASE', 'forge'),
+    'username' => env('DB_USERNAME', 'forge'),
+    'password' => env('DB_PASSWORD', ''),
+    'charset'  => 'utf8',
+    'prefix'   => '',
+    ],
 
     ],
 
@@ -106,15 +118,15 @@ return [
 
     'redis' => [
 
-        'cluster' => false,
+    'cluster' => false,
 
-        'default' => [
-            'host' => env('REDIS_HOST', 'localhost'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => 0,
-        ],
+    'default' => [
+    'host'     => env('REDIS_HOST', 'localhost'),
+    'password' => env('REDIS_PASSWORD', null),
+    'port'     => env('REDIS_PORT', 6379),
+    'database' => 0,
+    ],
 
     ],
 
-];
+    ];
