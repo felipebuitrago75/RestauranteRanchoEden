3387568d433b67c1347de9b7b782dd3bdc4d88b3
pipeb83 <felipebuitrago75@gmail.com>
2016-11-08 11:02:42 -0500

    prueba login 1

--- app/Http/Controllers/UsuarioControlador.php
+++ app/Http/Controllers/UsuarioControlador.php
@@ -24,9 +24,25 @@ class UsuarioControlador extends Controller
         return view('login')->with('usuario',$user);
     	
     }
-    public function validor(){
-
-        return view('/home');
+    /**
+     * Valida los datos del usuario.
+     */
+    public function postLogin()
+    {
+        // Guardamos en un arreglo los datos del usuario.
+        $userdata = array(
+            'email' => Input::get('email'),
+            'password'=> Input::get('password')
+        );
+        // Validamos los datos y además mandamos como un segundo parámetro la opción de recordar el usuario.
+        if(Auth::attempt($userdata, Input::get('remember-me', 0)))
+        {
+            // De ser datos válidos nos mandara a la bienvenida
+            return Redirect::to('/indicadores');
+        }
+        // En caso de que la autenticación haya fallado manda un mensaje al formulario de login y también regresamos los valores enviados con withInput().
+        return Redirect::to('login')
+                    ->with('mensaje_error', 'Tus datos son incorrectos')
+                    ->withInput();
     }
-
 }
