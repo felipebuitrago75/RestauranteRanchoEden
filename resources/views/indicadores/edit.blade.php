9277dc2c36188b72dd0f81c4698bb8bdae0d4ff0
pipeb83 <felipebuitrago75@gmail.com>
2016-11-09 14:28:33 -0500

    Prueba final editar

---
+++ resources/views/indicadores/edit.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+@section('contenido')
+<div class="container">
+	<div class="row">
+		<div class="col-md-10 col-md-offset-1">
+			<div class="panel panel-default">
+				<div class="panel-heading"> Editar indicador: <br>  {{$indicador-> nombre}}</div>
+					<div class="panel-body">
+
+
+
+						
+					{!! Form::model($indicador,['route' => ['indicadores.update',$indicador ],'method' =>'PUT']) !!}
+					<input type="hidden" name="_token" value="{{ csrf_token() }}">
+					   <div class="form-group">
+						   {!! Form::label('nombre','Nombre del indicador')!!}
+						   {!! Form::text('nombre',null,['class'=> 'form-control'])!!}
+
+						  </div>
+						  <div class="form-group">
+						   {!! Form::label('descripcion','Descripcion del indicador')!!}
+						   {!! Form::textarea('descripcion',null,['class'=> 'form-control','style' => 'resize:none'])!!}
+
+						  </div>
+					
+						 
+						  <button type="submit" class="btn btn-default">Actualizar</button>
+					{!! Form::close() !!}
+
+
+
+
+				
+					
+					
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+@endsection
