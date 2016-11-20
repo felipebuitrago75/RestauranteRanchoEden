ddc866f79f6a0f06328812c54187141e4dcfe598
ERICKVH66 <erickwith@hotmail.com>
2016-11-20 14:28:06 -0500

    commit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,13 +19,12 @@ Route::get('/', function () {
 Route::group(['middleware' => 'auth'], function () {
     Route::auth();
     Route::get('/admin', 'HomeController@index');
- 
 });
 
  Route::post('/auth/login', 'Auth\AuthController@postLogin');
+ 
 Route::resource('usuario', 'UsuarioControlador');
 Route::resource('indicadores','IndicadorControlador');
-
 Route::resource('tareas','tareaControlador');
 
 Route::get('/home', 'HomeController@index');
