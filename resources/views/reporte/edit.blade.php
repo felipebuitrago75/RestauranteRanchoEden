69cdcb0d661d711c873883545fe5c6346f6a142c
ERICKVH66 <erickwith@hotmail.com>
2016-11-20 17:56:19 -0500

    Reporte 1.2

--- resources/views/reporte/edit.blade.php
+++ resources/views/reporte/edit.blade.php
@@ -5,36 +5,7 @@
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
 				<div class="panel-heading"> Nombre de indicador: <br>  {{$indicador-> nombre}}</div>
-					  <div class="panel-body">
-					
-					  Listado de Tareas
-					  <p>
-					    
-					  </p>
-					  <p>hay {{$tareas->total()}} registros</p>
-					    <table class="table table-striped">
-					      <tr>
-					        <th>Id</th>
-					        <th>Descripcion</th>
-					        <th>Estado</th>
-					       
-					      </tr>
-					      @foreach ($indicadores as $indicador)
-					      @foreach ($tareas as $tarea)
-					     @if(($indicador->id_indicador)=== $tarea->indicador_id_indicador)
-
-					      <tr>
-					        <td>{{$tarea->id_tarea}}</td>                 
-					        <td>{{$tarea->descripcion}}</td>  
-					        <td>{{$tarea->estado}}</td>  
-
-
-					      </tr>
-					      @endif
-					      @endforeach
-					      @endforeach
-					    </table>
-					  {!! $tareas->render() !!}
+					  
 					</div>
 						  <div class="form-group">
 						   <p style="text-align: center;">TABLA DE ANALISIS DE INDICADOR</p>
