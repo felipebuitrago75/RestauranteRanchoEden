17f679bbe07292038d7c0baf85a059978dfbc351
ERICKVH66 <erickwith@hotmail.com>
2016-11-20 16:08:02 -0500

    Error- Elementos de apoyo
    En este comit se evidencia la correcion del error "elemntos de apoyo"

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,3 +27,4 @@ Route::resource('usuario', 'UsuarioControlador');
 Route::resource('indicadores','IndicadorControlador');
 
 Route::resource('tareas','tareaControlador');
+Route::resource('reporte','ReporteControlador');
