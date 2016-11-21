42b1e3a3fd8d02bc4e63706054841861d6e41820
pipeb83 <felipebuitrago75@gmail.com>
2016-11-21 00:11:51 -0500

    cambio en controlador

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -29,7 +29,7 @@ class AuthController extends Controller
      * @var string
      */
     protected $redirectTo = '/indicadores';
-    protected $username = 'email';
+  
     /**
      * Create a new authentication controller instance.
      *
