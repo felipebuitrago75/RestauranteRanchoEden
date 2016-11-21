090702bf7d01455f7421e6f4ed0bf08105c8a2a7
pipeb83 <felipebuitrago75@gmail.com>
2016-11-21 00:47:36 -0500

    Cambios en modelo y auth

--- app/Usuario.php
+++ app/Usuario.php
@@ -10,6 +10,9 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Usuario extends Model
 {
+    public function setPasswordAttribute($password){
+    $this->attributes['password'] = Hash::needsRehash($password) ? Hash::make($password) : $password;
+}
     // variable que almacena la tabla usuario proveniente de la base de datos.
     protected $table= 'usuario';
 
