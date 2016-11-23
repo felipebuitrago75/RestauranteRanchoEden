6bce7f932ca0c298a9a31d6b58032ee6e4cf8a46
ERICKVH66 <erickwith@hotmail.com>
2016-11-22 19:24:36 -0500

    Excel 1.3

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -10,8 +10,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-use Barryvdh\DomPDF\PDF;
-use PDF;
 use Excel;
 use App\Indicador;
 
