47caa0dd29bbc27b04ca5a8894917cc12633b553
ERICKVH66 <erickwith@hotmail.com>
2016-11-22 21:57:18 -0500

    Grafica 8

--- resources/views/indicadores/index.blade.php
+++ resources/views/indicadores/index.blade.php
@@ -35,7 +35,7 @@
                 <td>
                   <a href="{{URL::action('tareaControlador@edit',$indicador->id_indicador)}}">Agregar Tareas</a>
                   <a href="{{URL::action('IndicadorControlador@edit',$indicador->id_indicador)}}">Editar Indicador</a>
-                  <a href="{{URL::action('ReporteControlador@edit',$indicador->id_indicador)}}">Reporte</a>
+                  <a href="{{URL::action('ChartController@index',$indicador->id_indicador)}}">Reporte</a>
 
              
                 </td>
