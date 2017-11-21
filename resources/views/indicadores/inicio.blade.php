35cbfcc146011b51433ca2ba45b8a4f5a34f95c3
ERICKVH66 <erickwith@hotmail.com>
2017-11-21 11:39:04 -0500

    Actualizacion reporte

--- resources/views/indicadores/inicio.blade.php
+++ resources/views/indicadores/inicio.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.appInicio')
-@section('contenido')
+@section('contenido2')
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
--- resources/views/layouts/appInicio.blade.php
+++ resources/views/layouts/appInicio.blade.php
@@ -90,7 +90,7 @@
                 <div class="col-md-12">
                     <div class="box">
                         <div class="box-header with-border">
-                            <h3 class="box-title">SISTEMA DE GESTIÓN DE INDICADORES AMBIENTALES : RESTAURANTE RANCHO EDEN</h3>
+                            <h3 class="box-title">SISTEMA DE GESTIÓN DE INDICADORES AMBIENTALES : RESTAURANTE RANCHO ED�N</h3>
                             <div class="box-tools pull-right">
                                 <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
 
@@ -102,7 +102,7 @@
                             <div class="row">
                                 <div class="col-md-12">
                                     <!--Contenido-->
-                                @yield('contenido')
+                                @yield('contenido2')
 
                                 <!--Fin Contenido-->
                                 </div>
