05858eb914f1c5e6d7cd2507eea2a98840b1e12e
ERICKVH66 <erickwith@hotmail.com>
2017-11-21 09:29:21 -0500

    Actualizacion login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
       <header class="main-header">
 
         <!-- Logo -->
-        <a href="{{route('indicadores.index')}}" class="logo">
+        <a href="{{route ('/')}}" class="logo">
           <!-- mini logo for sidebar mini 50x50 pixels -->
           <span class="logo-mini"><b></b>V</span>
           <!-- logo for regular state and mobile devices -->
--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -67,7 +67,7 @@ Landing page based on Pratt: http://blacktie.co/demo/pratt/
                     <li><a href="{{ url('/login') }}">{{ trans('adminlte_lang::message.login') }}</a></li>
                     <li><a href="{{ url('/register') }}">{{ trans('adminlte_lang::message.register') }}</a></li>
                 @else
-                    <li><a href="/home">{{ Auth::user()->name }}</a></li>
+                    <li><a href="/indicadores">{{ Auth::user()->name }}</a></li>
                 @endif
             </ul>
 
