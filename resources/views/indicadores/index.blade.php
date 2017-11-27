d8b7a45bdce5257f452ff439e2d430e139861d09
ERICKVH66 <erickwith@hotmail.com>
2017-11-26 19:29:20 -0500

    Logos

--- resources/views/indicadores/index.blade.php
+++ resources/views/indicadores/index.blade.php
@@ -33,10 +33,9 @@
               <a class="btn btn-info" href="{{URL::action('tareaControlador@index',$indicador->id_indicador)}}" role="button">Tareas</a>
 
                 <td>
-                 <a href="{{URL::action('tareaControlador@edit',$indicador->id_indicador)}}">Agregar Tareas</a>
-                  <a href="{{URL::action('IndicadorControlador@edit',$indicador->id_indicador)}}">Editar Indicador</a>
-                  <a href="{{URL::action('ReporteControlador@edit',$indicador->id_indicador)}}">Reporte</a>
-
+                <a href="{{URL::action('tareaControlador@edit',$indicador->id_indicador)}}"><img src="img/agregarTarea.png" title="Agregar tarea"></a>
+                  <a href="{{URL::action('IndicadorControlador@edit',$indicador->id_indicador)}}"><img src="img/logoEditarIndicador.png" title="Editar indicador"></a>
+                  <a href="{{URL::action('ReporteControlador@edit',$indicador->id_indicador)}}"><img src="img/reporteIndicador.png" title="Reporte"></a>
              
                 </td>
 
