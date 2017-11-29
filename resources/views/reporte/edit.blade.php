8e2501fc22252f72cf4161d6c54c8cae2244c962
ERICKVH66 <erickwith@hotmail.com>
2017-11-29 18:57:39 -0500

    Grafico de barras

--- resources/views/reporte/edit.blade.php
+++ resources/views/reporte/edit.blade.php
@@ -139,6 +139,48 @@
 						  	  </body>
 						  	</html>
 						  </div>>
+						  <div>
+                        <html>
+                        <head>
+                            <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+                            <script type="text/javascript">
+                                google.charts.load('current', {'packages':['bar']});
+                                google.charts.setOnLoadCallback(drawStuff);
+
+                                function drawStuff() {
+                                    var data = new google.visualization.arrayToDataTable([
+                                        ['Tareas', 'Progreso'],
+                                            @foreach ($tareas as $tarea)
+                                        ['{{$tarea->descripcion}}',    {{$tarea->estado}}],
+                                        @endforeach
+
+                                    ]);
+
+                                    var options = {
+                                        width: 800,
+                                        legend: { position: 'none' },
+                                        chart: {
+                                            title: 'Grafico de barras',
+                                            subtitle: 'Progreso de tareas' },
+                                        axes: {
+                                            x: {
+                                                0: { side: 'top', label: 'Tareas'} // Top x-axis.
+                                            }
+                                        },
+                                        bar: { groupWidth: "90%" }
+                                    };
+
+                                    var chart = new google.charts.Bar(document.getElementById('top_x_div'));
+                                    // Convert the Classic options to Material options.
+                                    chart.draw(data, google.charts.Bar.convertOptions(options));
+                                };
+                            </script>
+                        </head>
+                        <body>
+                        <div id="top_x_div" style="width: 800px; height: 600px;"></div>
+                        </body>
+                        </html>
+                    </div>
 					
 						 
 					  <a class="btn btn-info" href="{{ route('reporte.edit', $indicador->id) }}" role="button"> Descargar</a>
