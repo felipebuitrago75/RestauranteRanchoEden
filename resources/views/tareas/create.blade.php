e8267d067ef686d23b879c03b043c40afd00858a
ERICKVH66 <erickwith@hotmail.com>
2018-02-12 18:51:42 -0500

    Actualizacion tareas

--- resources/views/tareas/create.blade.php
+++ resources/views/tareas/create.blade.php
@@ -4,25 +4,30 @@
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
-				<div class="panel-heading"> Editar Tarea <br> Tarea:  {{$tarea-> descripcion}}</div>
+				<div class="panel-heading">  <br> Tarea:  {{$tarea-> descripcion}}</div>
 					<div class="panel-body">
 
 
 
-						{!! Form::model($tarea,['route' => ['tareas.update',$tarea ],'method' =>'PUT']) !!}
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-					   <div class="form-group">
-						   {!! Form::label('descripcion','Nombre de la tarea')!!}
-						   {!! Form::text('descripcion',null,['class'=> 'form-control'])!!}
+						<form action="{{ route('tareas.store') }}" method="post">
+							<input type="hidden" name="_token" value="{{ csrf_token() }}">
+							<div class="form-group">
+								{!! Form::label('descripcion','Nombre de la tarea')!!}
+								{!! Form::text('descripcion',null,['class'=> 'form-control'])!!}
 
-						  </div>
-						  <div class="form-group">
-						   {!! Form::label('estado','Estado de la tarea')!!}
-						   {!! Form::text('estado',null,['class'=> 'form-control','style' => 'resize:none'])!!}
+							</div>
+							<div class="form-group">
+								{!! Form::label('estado','Estado de la tarea')!!}
+								{!! Form::text('estado',null,['class'=> 'form-control','style' => 'resize:none'])!!}
 
-						  </div>
-						<button type="submit" class="btn btn-default">Actualizar</button>
-						{!! Form::close() !!}
+							</div>
+							<div>
+								<input type="hidden" name="id_indicador" value="{{$indicador->id_indicador}}">
+							</div>
+
+
+							<button type="submit" class="btn btn-default">Registrar</button>
+						</form>
 				</div>
 			</div>
 		</div>
