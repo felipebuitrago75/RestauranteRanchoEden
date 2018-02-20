c729c64e4449bbddd3dee612ca4556ce034fdb0f
ERICKVH66 <erickwith@hotmail.com>
2018-02-20 18:37:07 -0500

    copyright

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -150,7 +150,7 @@
         <div class="pull-right hidden-xs">
           <b>Version</b> 2.3.0
         </div>
-        <strong>Copyright &copy; 2016-2020 </strong>Proyecto Final SW 3
+       <strong>Copyright &copy; 2018 - Restaurante Rancho Edén S.A.S Todos los derechos reservados. </strong>
       </footer>
 
       
