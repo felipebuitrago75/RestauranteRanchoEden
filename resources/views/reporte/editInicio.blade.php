1aa6fe94293a6c04c59df1dedad122416d6bf637
ERICKVH66 <erickwith@hotmail.com>
2018-04-25 12:32:07 -0500

    REPORTES TABLA

--- resources/views/reporte/editInicio.blade.php
+++ resources/views/reporte/editInicio.blade.php
@@ -16,92 +16,60 @@
                     <table align="center" border="1" cellpadding="1" cellspacing="1" id="TablaIndicador" style="width:500px;">
                         <tbody>
                         <tr>
-
-                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong>A&Ntilde;O 2015</strong></td>
-                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong>A&Ntilde;O 2016</strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong>INDICADOR</strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong>PERIODO I [ENERO-FEBRERO]</strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong>PERIODO II [FEBRERO-MARZO]</strong></td>
                             <td style="text-align: center; border-color: rgb(51, 153, 51);"><strong>AHORRO </strong></td>
                         </tr>
                         <tr>
-
-                            <td style="border-color: rgb(51, 153, 51);"><strong>ENERO</strong></td>
-                            <td style="border-color: rgb(51, 153, 51);"><strong>FEBRERO</strong></td>
-                            <td style="border-color: rgb(51, 153, 51);"><strong>ENERO</strong></td>
-                            <td style="border-color: rgb(51, 153, 51);"><strong>FEBRERO</strong></td>
-                            <td style="border-color: rgb(51, 153, 51);">&nbsp;</td>
-
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong>Residuos totales producidos por cada área</strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong></strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong></strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong></strong></td>
                         </tr>
                         <tr>
-                            <td style="border-color: rgb(51, 153, 51);">95000</td>
-                            <td style="border-color: rgb(51, 153, 51);">99000</td>
-                            <td style="border-color: rgb(51, 153, 51);">74000</td>
-                            <td style="border-color: rgb(51, 153, 51);">80000</td>
-                            <td style="border-color: rgb(51, 153, 51);">&nbsp;</td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong>Consumo de envases de vidrio y plástico</strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong></strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong></strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong></strong></td>
                         </tr>
                         <tr>
-                            <td style="border-color: rgb(51, 153, 51);"><strong>FEBRERO</strong></td>
-                            <td style="border-color: rgb(51, 153, 51);"><strong>MARZO</strong></td>
-                            <td style="border-color: rgb(51, 153, 51);"><strong>FEBRERO</strong></td>
-                            <td style="border-color: rgb(51, 153, 51);"><strong>MARZO</strong></td>
-                            <td style="border-color: rgb(51, 153, 51);">&nbsp;</td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong>Consumo de papel-cartón</strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong></strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong></strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong></strong></td>
 
                         </tr>
                         <tr>
-                            <td style="border-color: rgb(51, 153, 51);">95000</td>
-                            <td style="border-color: rgb(51, 153, 51);">99000</td>
-                            <td style="border-color: rgb(51, 153, 51);">74000</td>
-                            <td style="border-color: rgb(51, 153, 51);">80000</td>
-                            <td style="border-color: rgb(51, 153, 51);">&nbsp;</td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong>Tasa de gestión de aceites usados</strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong></strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong></strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong></strong></td>
                         </tr>
                         <tr>
-                            <td style="border-color: rgb(51, 153, 51);"><strong>MARZO</strong></td>
-                            <td style="border-color: rgb(51, 153, 51);"><strong>ABRIL</strong></td>
-                            <td style="border-color: rgb(51, 153, 51);"><strong>MARZO</strong></td>
-                            <td style="border-color: rgb(51, 153, 51);"><strong>ABRIL</strong></td>
-                            <td style="border-color: rgb(51, 153, 51);">&nbsp;</td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong>Disposición (destino) de los residuos.</strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong></strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong></strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong></strong></td>
 
                         </tr>
                         <tr>
-                            <td style="border-color: rgb(51, 153, 51);">95000</td>
-                            <td style="border-color: rgb(51, 153, 51);">99000</td>
-                            <td style="border-color: rgb(51, 153, 51);">74000</td>
-                            <td style="border-color: rgb(51, 153, 51);">80000</td>
-                            <td style="border-color: rgb(51, 153, 51);">&nbsp;</td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong>Consumo total de agua</strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong></strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong></strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong></strong></td>
                         </tr>
                         <tr>
-                            <td style="border-color: rgb(51, 153, 51);"><strong>ABRIL</strong></td>
-                            <td style="border-color: rgb(51, 153, 51);"><strong>MAYO</strong></td>
-                            <td style="border-color: rgb(51, 153, 51);"><strong>ABRIL</strong></td>
-                            <td style="border-color: rgb(51, 153, 51);"><strong>MAYO</strong></td>
-                            <td style="border-color: rgb(51, 153, 51);">&nbsp;</td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong>Gasto en gestión de aguas residuales</strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong></strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong></strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong></strong></td>
                         </tr>
                         <tr>
-                            <td style="border-color: rgb(51, 153, 51);">95000</td>
-                            <td style="border-color: rgb(51, 153, 51);">99000</td>
-                            <td style="border-color: rgb(51, 153, 51);">74000</td>
-                            <td style="border-color: rgb(51, 153, 51);">80000</td>
-                            <td style="border-color: rgb(51, 153, 51);">&nbsp;</td>
-                        </tr>
-                        <tr>
-                            <td style="border-color: rgb(51, 153, 51);"><strong>MAYO</strong></td>
-                            <td style="border-color: rgb(51, 153, 51);"><strong>JUNIO</strong></td>
-                            <td style="border-color: rgb(51, 153, 51);"><strong>MAYO</strong></td>
-                            <td style="border-color: rgb(51, 153, 51);"><strong>JUNIO</strong></td>
-                            <td style="border-color: rgb(51, 153, 51);">&nbsp;</td>
-                        </tr>
-                        <tr>
-                            <td style="border-color: rgb(51, 153, 51);">95000</td>
-                            <td style="border-color: rgb(51, 153, 51);">99000</td>
-                            <td style="border-color: rgb(51, 153, 51);">74000</td>
-                            <td style="border-color: rgb(51, 153, 51);">80000</td>
-                            <td style="border-color: rgb(51, 153, 51);">&nbsp;</td>
-                        </tr>
-                        <tr>
-                            <td style="border-color: rgb(51, 153, 51);">TOTAL</td>
-                            <td style="border-color: rgb(51, 153, 51);">&nbsp;</td>
-                            <td style="border-color: rgb(51, 153, 51);">&nbsp;</td>
-                            <td style="border-color: rgb(51, 153, 51);">&nbsp;</td>
-                            <td style="border-color: rgb(51, 153, 51);">&nbsp;</td>
-
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong>Consumo de energía primaria</strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong></strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong></strong></td>
+                            <td colspan="2" style="text-align: center; border-color: rgb(51, 153, 51);"><strong></strong></td>
                         </tr>
                         </tbody>
                     </table>
