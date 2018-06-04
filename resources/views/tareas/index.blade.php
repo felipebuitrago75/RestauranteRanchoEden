5ffabd4aa89dafdfadfe3a4423163078171068ee
ERICKVH66 <erickwith@hotmail.com>
2018-06-04 18:24:46 -0500

    paginacion 2

--- resources/views/tareas/index.blade.php
+++ resources/views/tareas/index.blade.php
@@ -11,28 +11,27 @@
             
           </p>
           <p></p>
+          <p>hay {{$tareas->total()}} registros</p>
             <table class="table table-striped">
               <tr>
                 <th>Id</th>
                 <th>Descripcion</th>
                 <th>Progreso (%)</th>
                  <th>Acciones</th>
-
-               
               </tr>
+
               @foreach ($tareas as $tarea)
              @if($indicadores->id_indicador == $tarea->indicador_id_indicador)
-
               <tr>
                 <td>{{$tarea->id_tarea}}</td>                 
                 <td>{{$tarea->descripcion}}</td>  
                  <td><progress max="100" value= "{{$tarea->estado}}" ></progress></td>  
                  <td> <a href="{{URL::action('tareaControladorEditar@edit',$tarea->id_tarea)}}">Editar tarea</a></td>
 
-
               </tr>
               @endif
               @endforeach
+
             </table>
           {!! $tareas->render() !!}
         </div>
